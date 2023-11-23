4c3866c27fa583177e7583f56802894dda6ceb65
JeeIn-Park <1700pji@naver.com>
2023-11-23 09:25:45 +0000

    misunderstood algorithm

--- cw_2176132.pl
+++ cw_2176132.pl
@@ -10,12 +10,20 @@ solve_task(Task,Cost) :-
 % User-defined comparison predicate with an additional parameter
 compare_f(Task, Order, List1, List2) :-
     (   compare_property(Task, List1, List2) -> Order = (<)
-    ;   compare_property(Task, List1, List2) -> Order = (>)
+    ;   compare_property(Task, List2, List1) -> Order = (>)
     ;   Order = (=)
     ).
 
 % Auxiliary predicate to compare elements based on a property and an extra parameter
 compare_property(Task, List1, List2) :-
+    Task = p(TaskX, TaskY),
+    List1 = [p(X1, Y1)|_], List2 = [p(X2, Y2)|_],
+    abs(X1 - TaskX, AbsX1), abs(Y1 - TaskY, AbsY1),
+    abs(X2 - TaskX, AbsX2), abs(Y2 - TaskY, AbsY2),
+    length(List1, G1), length(List2, G2),
+    F1 is G1 + AbsX1 + AbsY1,
+    F2 is G2 + AbsX2 + AbsY2,
+    F1 < F2.
     % Your comparison logic here based on some_param
     % For example, comparing elements based on some property
     % This could involve some specific checks, computations, or conditions
@@ -23,27 +31,7 @@ compare_property(Task, List1, List2) :-
 
 % Sorting a list using predsort with the user-defined comparison predicate with an extra parameter
 sort_lists_by_f(Task, Queue, PriorityQueue) :-
-    predsort(compare_f(Task), Queue, PriorityQueue).
-
-
-% Comparator predicate to compare lists based on their lengths
-compare_f(<, List1, List2) :-
-    Task = p(TaskX, TaskY),
-    Queue = [Next|Rest],
-    Next = [Pos|RPath],
-    length(List1, Len1),
-    length(List2, Len2),
-    Len1 < Len2.
-compare_f(>, List1, List2) :-
-    Task = p(TaskX, TaskY),
-    length(List1, Len1),
-    length(List2, Len2),
-    Len1 > Len2.
-compare_f(=, _, _). % If the lengths are equal, keep the order unchanged
-
-% Predicate to sort a list of lists based on their lengths
-sort_lists_by_f(Task, Queue, PriorityQueue) :-
-    predsort(compare_f, Queue, PriorityQueue).
+    predsort(compare_f(Task), Queue, PriorityQueue), write(PriorityQueue).
 
 
 % Calculate the path required to achieve a Task
@@ -57,26 +45,7 @@ solve_task_bfs(Task, Queue, Visited, Path) :-
                             Newfound),
                             append(Rest,Newfound,NewQueue),
                             sort_lists_by_f(Task, NewQueue, PriorityQueue),
-                            
-                            Task = p(TaskX, TaskY),
-                            Queue = [Next|Rest],
-                            Next = [Pos|RPath],
-                            NP = p(NPX, NPY),
-                            abs(NPX - TaskX, AbsX), abs(NPY - TaskY, AbsY),
-                            F is G + AbsX + AbsY,
-
-
-
-
-        % Calculate f(n) = g(n) + h(n) for each node NP
-        % Use priority queue based on f(n) to guide the search,
-        % where g(n) is the cost to reach NP so far and h(n) is the heuristic cost from NP to Goal.
-        % Update NewQueue with the priority queue ordering nodes based on f(n).
-
-                      
-
-
-                      solve_task_bfs(Task, PriorityQueue,[Pos|Visited],Path))).
+                             solve_task_bfs(Task, PriorityQueue,[Pos|Visited],Path))).
 
 
 
