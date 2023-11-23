7fbbfd088164b8dcb8504777b0284a4d4d52a54c
JeeIn-Park <1700pji@naver.com>
2023-11-23 22:07:00 +0000

    .

--- cw_2176132.pl
+++ cw_2176132.pl
@@ -2,28 +2,46 @@
 solve_task(Task,Cost) :-
     my_agent(A), 
     get_agent_position(A,P),
-    solve_task_bfs(Task,[[P]],[],[P|Path]), !,
+    heuristic(P, Task, Heuristic),
+    solve_task_as(Task,[[Heuristic, P]],[],[P|Path]), !,
 
     agent_do_moves(A,Path), 
     length(Path,Cost).
 
+
+% Comparator predicate to compare lists based on their first element
+compare_first_element(<, [X1|_], [X2|_]) :-
+    X1 @< X2. % Sort based on the first element in ascending order
+compare_first_element(>, [X1|_], [X2|_]) :-
+    X1 @> X2. % Sort based on the first element in descending order
+compare_first_element(=, _, _). % If the first elements are equal, keep the order unchanged
+
+% Predicate to sort a list of lists based on their first element
+sort_lists_by_first_element(Lists, SortedLists) :-
+    predsort(compare_first_element, Lists, SortedLists).
+
+
+
 % Calculate the path required to achieve a Task
-solve_task_bfs(Task, Queue, Visited, Path) :-
+solve_task_as(Task, Queue, Visited, Path) :-
     Queue = [Next|Rest],
     Next = [_, Pos|RPath],
     (achieved(Task, Pos) -> reverse([Pos|RPath],Path)
-
-    ;otherwise           -> (findall([NP, Pos|RPath],
-                            (map_adjacent(Pos,NP,empty), \+ member(NP,Visited), \+ member([NP|_], Rest)),
+    ;otherwise           -> (findall([NF, NP, Pos|RPath],
+                            (map_adjacent(Pos,NP,empty), heuristic(NP, Task, NF), \+ member(NP,Visited), \+ member([NP|_], Rest)),
                             Newfound),
                             append(Newfound,Rest,NewQueue),
-                            solve_task_bfs(Task, NewQueue,[Pos|Visited],Path))).
+                            sort_lists_by_first_element(NewQueue, PriorityQueue),
+                            write(PriorityQueue),
+                            solve_task_as(Task, PriorityQueue,[Pos|Visited],Path))).
 
 
-heuristic(NextPos, Goal, Heuristic) :- 
-    (Goal = go(GoalPos), ground(GoalPos), !, 
-    map_distance(NextPos, GoalPos, Heuristic); 
+heuristic(Pos, Task, Heuristic) :- 
+    (Task = go(GoalPos), !, map_distance(Pos, GoalPos, Heuristic)
+    ; 
     Heuristic = 0).
+
+
 
 % True if the Task is achieved with the agent at Pos
 achieved(Task,Pos) :- 
