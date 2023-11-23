5ecf8bc8422f5e7e8e3f04326a4db8d5d6e0828d
JeeIn-Park <1700pji@naver.com>
2023-11-23 21:19:14 +0000

    before h

--- cw_2176132.pl
+++ cw_2176132.pl
@@ -7,49 +7,26 @@ solve_task(Task,Cost) :-
     agent_do_moves(A,Path), 
     length(Path,Cost).
 
-% User-defined comparison predicate with an additional parameter
-compare_f(Task, Order, List1, List2) :-
-    (   compare_property(Task, List1, List2) -> Order = (<)
-    ;   compare_property(Task, List2, List1) -> Order = (>)
-    ;   Order = (=)
-    ).
-
-% Auxiliary predicate to compare elements based on a property and an extra parameter
-compare_property(Task, List1, List2) :-
-    Task = p(TaskX, TaskY),
-    List1 = [p(X1, Y1)|_], List2 = [p(X2, Y2)|_],
-    abs(X1 - TaskX, AbsX1), abs(Y1 - TaskY, AbsY1),
-    abs(X2 - TaskX, AbsX2), abs(Y2 - TaskY, AbsY2),
-    length(List1, G1), length(List2, G2),
-    F1 is G1 + AbsX1 + AbsY1,
-    F2 is G2 + AbsX2 + AbsY2,
-    F1 < F2.
-    % Your comparison logic here based on some_param
-    % For example, comparing elements based on some property
-    % This could involve some specific checks, computations, or conditions
-    % Return true if Elem1 should come before Elem2 based on some_param.
-
-% Sorting a list using predsort with the user-defined comparison predicate with an extra parameter
-sort_lists_by_f(Task, Queue, PriorityQueue) :-
-    predsort(compare_f(Task), Queue, PriorityQueue).
-
-
 % Calculate the path required to achieve a Task
 solve_task_bfs(Task, Queue, Visited, Path) :-
     Queue = [Next|Rest],
-    Next = [Pos|RPath],
+    Next = [_, Pos|RPath],
     (achieved(Task, Pos) -> reverse([Pos|RPath],Path)
 
-    ;otherwise           -> (findall([NP,Pos|RPath],
-                            (map_adjacent(Pos,NP,empty),\+ member(NP,Visited), \+ member([NP|_],Rest)),
+    ;otherwise           -> (findall([NP, Pos|RPath],
+                            (map_adjacent(Pos,NP,empty), \+ member(NP,Visited), \+ member([NP|_], Rest)),
                             Newfound),
-                            append(Rest,Newfound,NewQueue),
-                            sort_lists_by_f(Task, NewQueue, PriorityQueue),
-                            solve_task_bfs(Task, PriorityQueue,[Pos|Visited],Path))).
+                            append(Newfound,Rest,NewQueue),
+                            solve_task_bfs(Task, NewQueue,[Pos|Visited],Path))).
 
+
+heuristic(NextPos, Goal, Heuristic) :- 
+    (Goal = go(GoalPos), ground(GoalPos), !, 
+    map_distance(NextPos, GoalPos, Heuristic); 
+    Heuristic = 0).
 
 % True if the Task is achieved with the agent at Pos
 achieved(Task,Pos) :- 
-    Task=find(Obj), map_adjacent(Pos,_,Obj)
+    Task = find(Obj), map_adjacent(Pos,_,Obj)
     ;
-    Task=go(Pos).
+    Task = go(Pos).
