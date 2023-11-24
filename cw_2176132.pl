2e1a439829cb8418135b6af9d5d820eb58ef2767
JeeIn-Park <1700pji@naver.com>
2023-11-24 00:15:11 +0000

    state

--- cw_2176132.pl
+++ cw_2176132.pl
@@ -30,58 +30,23 @@ solve_task_as(Task, Queue, Visited_node, Path) :-
             solve_task_as(Task, Priority_queue, [Current_Position|Visited_node], Path)
         )
     ).
-    
-    
-
-search_bf(Queue, Visited, Path) :-
-    Queue = [Next|Rest],
-    Next = [Pos|RPath],
-    (complete(Current) -> reverse([Pos|RPath], Path)
-    ;otherwise -> (
-        findall([NP,Pos|RPath],
-        (map_adjacent(Pos, NP, empty),
-        \+member(NP, Visited),
-        \+member([NP|_], Rest)
-        ), NewFound),
-        append(Rest, NewFound, NewQueue), 
-        search_bf(NewQueue, [Pos|Visited], Path))
-    ).
-    
-    
-    Queue = [Next|Rest],
-    Next = [_, Pos|RPath],
-    (achieved(Task, Pos) -> reverse([Pos|RPath],Path)
-    ; otherwise -> ( bagof([NF, NP, Pos|RPath], 
-            NF^(map_adjacent(Pos, NP, empty), heuristic(NP, Task, NH), length(Next, NG), NF is NH + NG,
-                \+ member(NP, Visited),
-                \+ member([NP|_], Rest)),
-            Newfound),
-        append(Newfound, Rest, NewQueue),
-        sort_lists_by_first_element(NewQueue, PriorityQueue),
-        %write(PriorityQueue),
-        solve_task_as(Task, PriorityQueue, [Pos|Visited], Path)
-    )).
 
 
-
-
-% Comparator predicate to compare lists based on their first element
-compare_first_element(<, [X1|_], [X2|_]) :-
-    X1 @< X2. % Sort based on the first element in ascending order
-compare_first_element(>, [X1|_], [X2|_]) :-
-    X1 @> X2. % Sort based on the first element in descending order
-compare_first_element(=, _, _). % If the first elements are equal, keep the order unchanged
-
-% Predicate to sort a list of lists based on their first element
-sort_lists_by_first_element(Lists, SortedLists) :-
-    predsort(compare_first_element, Lists, SortedLists), write(SortedLists).
-
-
-
-
-heuristic(NextPos, Goal, Heuristic) :-
-    ( Goal = go(GoalPos), ground(GoalPos), !, map_distance(NextPos, GoalPos, Heuristic)  % If Goal is go(Pos), use Manhattan distance to the GoalPosition
+heuristic(Next_position, Task, Heuristic) :-
+    ( Task = go(Goal_position), ground(Goal_position), !, map_distance(Next_position, Goal_position, Heuristic)
     ; Heuristic = 0 ).
+
+
+% Comparator predicate to compare states based on their F values
+compare_state_f(<, state(_, F1), state(_, F2)) :-
+    F1 < F2.
+compare_state_f(>, state(_, F1), state(_, F2)) :-
+    F1 > F2.
+compare_state_f(=, _, _).
+
+% Predicate to sort a list of states based on their F values
+sort_queue(Queue, Priority_queue) :-
+    predsort(compare_state_f, Queue, Priority_queue).
 
 
 % True if the Task is achieved with the agent at Pos
