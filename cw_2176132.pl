8a029c00b7bf57bac9ea4910b4b666f9b1e3724f
JeeIn-Park <1700pji@naver.com>
2023-11-24 00:10:37 +0000

    no 2D list

--- cw_2176132.pl
+++ cw_2176132.pl
@@ -2,11 +2,67 @@
 solve_task(Task,Cost) :-
     my_agent(A), 
     get_agent_position(A,P),
-    heuristic(P, Task, Heuristic),
-    solve_task_as(Task,[[Heuristic, P]],[],[P|Path]), !,
+    heuristic(P, Task, H),
+    solve_task_as(Task, [state([P], H)], [], [P|Path]), !,
 
     agent_do_moves(A,Path), 
     length(Path,Cost).
+
+
+% Calculate the path required to achieve a Task
+solve_task_as(Task, Queue, Visited_node, Path) :-
+    Queue = [state(Current_path, Estimated_cost)| Other_states],
+    Current_path = [Current_Position|Passed_Path],
+    (achieve(Task, Current_Position) 
+        -> reverse(Current_path, Path);
+
+    otherwise 
+        -> ( bagof( state([New_position|Current_path], F), F^
+                ( map_adjacent(Current_Position, New_position, empty), 
+                  heuristic(New_position, Task, H), length(Passed_Path, G), F is H + G,
+                  \+ member(New_position, Visited_node),
+                  \+ member([New_position|_], Queue)
+                ),
+                New_states
+            ),
+            append(Other_states, New_states, New_Queue),
+            sort_queue(New_Queue, Priority_queue),
+            solve_task_as(Task, Priority_queue, [Current_Position|Visited_node], Path)
+        )
+    ).
+    
+    
+
+search_bf(Queue, Visited, Path) :-
+    Queue = [Next|Rest],
+    Next = [Pos|RPath],
+    (complete(Current) -> reverse([Pos|RPath], Path)
+    ;otherwise -> (
+        findall([NP,Pos|RPath],
+        (map_adjacent(Pos, NP, empty),
+        \+member(NP, Visited),
+        \+member([NP|_], Rest)
+        ), NewFound),
+        append(Rest, NewFound, NewQueue), 
+        search_bf(NewQueue, [Pos|Visited], Path))
+    ).
+    
+    
+    Queue = [Next|Rest],
+    Next = [_, Pos|RPath],
+    (achieved(Task, Pos) -> reverse([Pos|RPath],Path)
+    ; otherwise -> ( bagof([NF, NP, Pos|RPath], 
+            NF^(map_adjacent(Pos, NP, empty), heuristic(NP, Task, NH), length(Next, NG), NF is NH + NG,
+                \+ member(NP, Visited),
+                \+ member([NP|_], Rest)),
+            Newfound),
+        append(Newfound, Rest, NewQueue),
+        sort_lists_by_first_element(NewQueue, PriorityQueue),
+        %write(PriorityQueue),
+        solve_task_as(Task, PriorityQueue, [Pos|Visited], Path)
+    )).
+
+
 
 
 % Comparator predicate to compare lists based on their first element
@@ -18,29 +74,14 @@ compare_first_element(=, _, _). % If the first elements are equal, keep the orde
 
 % Predicate to sort a list of lists based on their first element
 sort_lists_by_first_element(Lists, SortedLists) :-
-    predsort(compare_first_element, Lists, SortedLists).
+    predsort(compare_first_element, Lists, SortedLists), write(SortedLists).
 
 
 
-% Calculate the path required to achieve a Task
-solve_task_as(Task, Queue, Visited, Path) :-
-    Queue = [Next|Rest],
-    Next = [_, Pos|RPath],
-    (achieved(Task, Pos) -> reverse([Pos|RPath],Path)
-    ;otherwise           -> findall(List, bagof([NF, NP, Pos|RPath], 
-                            NF^(map_adjacent(Pos,NP,empty), heuristic(NP, Task, NH), length(RPath, NG), NF is NH + NG, \+ member(NP,Visited), \+ member([NP|_], Rest)), 
-                            List), Newfound),
-                            append(Newfound,Rest,NewQueue),
-                            sort_lists_by_first_element(NewQueue, PriorityQueue),
-                            write(PriorityQueue),
-                            solve_task_as(Task, PriorityQueue,[Pos|Visited],Path))).
 
-
-heuristic(Pos, Task, Heuristic) :- 
-    (Task = go(GoalPos), !, map_distance(Pos, GoalPos, Heuristic)
-    ; 
-    Heuristic = 0).
-
+heuristic(NextPos, Goal, Heuristic) :-
+    ( Goal = go(GoalPos), ground(GoalPos), !, map_distance(NextPos, GoalPos, Heuristic)  % If Goal is go(Pos), use Manhattan distance to the GoalPosition
+    ; Heuristic = 0 ).
 
 
 % True if the Task is achieved with the agent at Pos
