cbe66fa4dbe990390b52fb988525df7f19edd6d8
JeeIn-Park <1700pji@naver.com>
2023-11-25 13:32:01 +0000

    debugging energy

--- cw_2176132.pl
+++ cw_2176132.pl
@@ -3,23 +3,31 @@ solve_task(Task,Cost) :-
     my_agent(A), 
     get_agent_position(A,P),
     heuristic(P, Task, H),
-    solve_task_as(Task, [state([P], H)], [], [P|Path]), !,
+    solve_task_as(Task, A, [state([P], H)], [], [P|Path]), !,
 
     agent_do_moves(A,Path), 
     length(Path,Cost).
 
 
 % Calculate the path required to achieve a Task
-solve_task_as(Task, Queue, Visited_node, Path) :-
+solve_task_as(Task, A, Queue, Visited_node, Path) :-
     Queue = [state(Current_path, _)| Other_states],
     Current_path = [Current_Position|_],
     format('Current_Position: ~w~n', Current_Position),
     format('Queue           : ~w~n', [Queue]),
-    (achieved(Task, Current_Position) 
-        -> reverse(Current_path, Path) 
+    get_agent_energy(A, E),
+    length(Current_path, Estimate_energy_consumption),
+    Estimate_energy is E - Estimate_energy_consumption,
+    (Estinate_energy < 0
+    ->  solve_task(find(c(_)), _),
+        agent_topup_energy(A, c(_)),
+        get_agent_energy(A, Topuped_energy),
+        get_agent_position(A, New_starting_position),
+        solve_task_as(Task, A, [state([New_starting_position], H)], [], Path),
+    ;   (achieved(Task, Current_Position),
+        ->  reverse(Current_path, Path) 
         
-        ;
-        (   ( findall(state(New_state_element, F), (
+        ;   (( findall(state(New_state_element, F), (
                     New_state_element = [New_position|Current_path],
                     map_adjacent(Current_Position, New_position, empty),
                     heuristic(New_position, Task, H),
@@ -33,15 +41,17 @@ solve_task_as(Task, Queue, Visited_node, Path) :-
                 format('remain          : ~w~n', [Other_states]),
                 merge_and_sort_by_heuristic(Other_states, New_states, Priority_queue),
                 format('Priority_queue  : ~w~n', [Priority_queue]),
-                solve_task_as(Task, Priority_queue, [Current_Position|Visited_node], Path)
+                solve_task_as(Task, A, Priority_queue, [Current_Position|Visited_node], Path)
             )
-        ;   % Failure case of bagof (when bagof/3 doesn't find any new states)
-            format('No new states found. Proceeding with remaining states: ~w~n', [Other_states]),
-            solve_task_as(Task, Other_states, Visited_node, Path)
+            ;   % Failure case of bagof (when bagof/3 doesn't find any new states)
+                format('No new states found. Proceeding with remaining states: ~w~n', [Other_states]),
+                solve_task_as(Task, A, Other_states, Visited_node, Path)
+            )
         )
     ).
 
 
+%heuristic(+Next_position, +Task, -Heuristic)
 heuristic(Next_position, Task, Heuristic) :-
     ( Task = go(Goal_position), ground(Goal_position), !, map_distance(Next_position, Goal_position, Heuristic)
     ; Heuristic = 0 ).
@@ -54,7 +64,7 @@ merge_and_sort_by_heuristic(Queue,[Next|Rest],Priority_queue) :-
     merge_and_sort_by_heuristic(Updated,Rest,Priority_queue).
 
 
-% insert(+Queue,+Next,-Updated)
+% insert(+Queue,+Next,-Priority_queue)
 insert([],State,[State]).
 insert([state(Path, H)|Rest],state(New_path, NH),Priority_queue) :-
     (H >= NH -> Priority_queue=[state(New_path, NH),state(Path, H)|Rest]
@@ -62,8 +72,10 @@ insert([state(Path, H)|Rest],state(New_path, NH),Priority_queue) :-
                         Priority_queue=[state(Path, H)|Result]).
 
 
-% True if the Task is achieved with the agent at Pos
+% achieved(+Task,+Pos)
 achieved(Task,Pos) :- 
     Task = find(Obj), map_adjacent(Pos,_,Obj)
     ;
     Task = go(Pos).
+
+    
