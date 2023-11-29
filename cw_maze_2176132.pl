30397ab8aa5be9d8e64ee39ab204eac634ecf47f
JeeIn-Park <1700pji@naver.com>
2023-11-29 06:26:23 +0000

    going to change idea

--- cw_maze_2176132.pl
+++ cw_maze_2176132.pl
@@ -6,7 +6,6 @@ solve_maze :-
     solve_maze.
     
 
-%%%%%%%%%%%%%%%% USEFUL PREDICATES %%%%%%%%%%%%%%%%%%
 % Find a possible move for each agent
 find_moves([],[]).
 find_moves([A|As],[M|Moves]) :-
@@ -14,3 +13,49 @@ find_moves([A|As],[M|Moves]) :-
     random_member(M,PosMoves),
     find_moves(As,Moves).
 
+
+
+solve_maze_by_dfs() :-
+
+
+% True if A is a possible movement direction
+m(north). 
+m(east).
+m(south). 
+m(west).
+
+% True if p(X,Y) is on the board
+on_board(p(X,Y)) :- 
+    ailp_grid_size(S), X>=1, X=<S, Y>=1, Y=<S.
+%   ailp_grid_size(S), between(1,S,X), between(1,S,Y)
+
+% True if p(X1,Y1) is one step in direction M from p(X,Y) (no bounds check)
+pos_step(p(X,Y), west, p(X1,Y)) :- X1 is X-1.
+pos_step(p(X,Y), east, p(X1,Y)) :- X1 is X+1.
+pos_step(p(X,Y), north, p(X,Y1)) :- Y1 is Y-1.
+pos_step(p(X,Y), south, p(X,Y1)) :- Y1 is Y+1.
+
+% True if NPos is one step in direction M from Pos (with bounds check)
+new_pos(Pos,M,NPos) :-
+    on_board(Pos), pos_step(Pos, M, NPos), on_board(NPos).
+
+% True if a L has the same length as the number of squares on the board
+complete(L) :-
+    ailp_grid_size(N), N2 is N*N, length(L,N2).
+
+turn(east, south, clockwise).
+turn(south, west, clockwise).
+turn(west, north, clockwise).
+turn(north, east, clockwise).
+turn(D1, D2, anti_clockwise) := turn(D2, D1, clockwise).
+
+% Perform a sequence of moves creating a spiral pattern, return the moves as L
+
+% spiral - base case
+spiral(Ps,Qs,_) :- complete(Ps), !, reverse(Ps,Qs).
+
+% spiral - recursive case
+spiral([Q,P|Ps],Qs,S) :- new_pos(P,D,Q), (C=D ; turn(D,C,S)), new_pos(Q,C,R), \+ member(R,[P|Ps]), spiral([R,Q,P|Ps],Qs,S).
+
+% spiral - wrapper 
+spiral(Ps) :- my_agent(A), get_agent_position(A,P), new_pos(P,_,Q), spiral([Q,P],Ps,_).
--- cw_wp_2176132.pl
+++ cw_wp_2176132.pl
@@ -1,37 +1,35 @@
 % True if link L appears on A's Wikipedia page
-actor_has_link(L, T) :- 
-    actor(T), wp(T, WT), wt_link(WT, L).
+actor_has_link(L, A) :- 
+    actor(A), wp(A, WT), wt_link(WT, L).
 
 % Attempt to solve by visiting each oracle in ID order
-eliminate(_, _, [T], _, T) :- 
-    !, 
-    format('found identity : ~w~n', [T]).
-eliminate(G, ET, [T|Ts], Visited_oracle, Result) :-
-    format(' --- agent is exploring... '),
+% eliminate(Agent, Energy_threshild, Actor_list, Visited_oracle, Result) 
+eliminate(_, _, [A], _, A) :- 
+    format('found identity : ~w~n', [A]), !.
+eliminate(G, ET, [A|As], Visited_oracle, Result) :-
+    format(' --- agent is exploring... ~n'),
     get_agent_energy(G, E),
     (
         (E =< ET ->
             solve_task(find(c(_)), _), 
-            format(' --- charging ')
+            format(' --- charging ~n')
         ;
-            solve_task(find(o(ID)), _), %<- I want to make sure ID is not already in Visited oracle
-            format(' --- asking '),
+            solve_task(find(o(ID)), _),
+            format(' --- asking ~n'),
             agent_ask_oracle(G, o(ID), link, L),
-            format('--- link : ~w', [L]), % Added square brackets around L
-            actor_has_link(L, T),
-            format('agent was not : ~w~n', [T])
+            format('--- link : ~w~n', [L]), 
+            include(actor_has_link(L), [A|As], New_Ts)
         ),
-        
-        eliminate(G, ET, Ts, [N|Visited_oracle], Result)
+        eliminate(G, ET, New_Ts, [ID|Visited_oracle], Result)
     ).
 
 % Deduce the identity of the secret actor A
 find_identity(A) :- 
     format('going to check agent identity... ~n'),
-    findall(T, actor(T), Ts), 
-    format('possible identity list : ~w~n', [Ts]),
+    findall(A, actor(A), As), 
+    format('possible identity list : ~w~n', [As]),
     my_agent(G),
     ailp_grid_size(N),
     Energy_threshold is 2 * N, 
     format('identity checking just started. ~n'),
-    eliminate(G, Energy_threshold, Ts, [], A).
+    eliminate(G, Energy_threshold, As, [], A).
