df618e3fc30adf870b8dc6424b9d51488dbe8129
JeeIn-Park <1700pji@naver.com>
2023-11-30 04:30:12 +0000

    queue_waiting_list

--- cw_maze_2176132.pl
+++ cw_maze_2176132.pl
@@ -16,21 +16,21 @@ back(D1, D2) :- back(D2, D1).
 % initial call
 solve_maze :-
     my_agents([Agent|My_agents]),
-    evolve_state(state([entity(Agent, south)], [], My_agents, [], []), _).
+    evolve_state([entity(Agent, south)], state([], My_agents, [], []), _).
 
 
 % main loop
-evolve_state(State, New_state) :-
-    State = state(Entities, Move_queue, Available_agents, Waiting_list, Exploered_nodes),
+evolve_state(Entities, State, New_state) :-
+    State = state(Move_queue, Available_agents, Waiting_list, Exploered_nodes),
     for all entity (next_move)
     append(Free_agents, Available_agents, Arrived, New_available_agent)
     execute_move_queue
     evolve_state.
 
 
-
-next_move(State, Updated_State) :-
-    State = state([entity(ID, Going)|Entities], Move_queue, Available_agents, Waiting_list, Exploered_nodes),
+% recursive call updating entities
+next_move(entity(ID, Going), State, Updated_State) :-
+    State = state(Move_queue, Available_agents, Waiting_list, Exploered_nodes),
     get_agent_position(ID, Current_position),
     findall(path(New_position, Direction), 
         (agent_adjacent(ID, New_position, empty), 
@@ -38,11 +38,11 @@ next_move(State, Updated_State) :-
         \+ back(Going,Direction)),
         New_paths),
     (  New_paths = [path(Path_position, _)]
-    ->  Updated_State = state([entity(ID, Going)|Entities], [agent_move_queue(entity(ID, Going), [Path_position])|Move_queue], Available_agents, Waiting_list, Exploered_nodes)
+    ->  Updated_State = state([agent_move_queue(entity(ID, Going), [Path_position])|Move_queue], Available_agents, Waiting_list, Exploered_nodes)
     ;   (   New_paths = []
-        ->  Updated_State = state(Entities, Move_queue, [ID|Available_agents], Waiting_list, Exploered_nodes)
+        ->  Updated_State = state(Move_queue, [ID|Available_agents], Waiting_list, Exploered_nodes)
         ;   member(path(Step, Going), New_paths),
-            Updated_State = state([entity(ID, Going)|Entities], [agent_move_queue(entity(ID, Going), [Step])|Move_queue], Available_agents, Updated_waiting_list, Exploered_nodes),
+            Updated_State = state([agent_move_queue(entity(ID, Going), [Step])|Move_queue], Available_agents, Updated_waiting_list, Exploered_nodes),
             select(path(Step, Going), New_paths, _),
             append(New_paths, Waiting_list, Updated_waiting_list)
         )
@@ -50,26 +50,29 @@ next_move(State, Updated_State) :-
 
 
 
-Move calculator 
-% if there is waiting list and Available_agents, allocate available agent to waiting list traveling
 Explored_node calculator
 % when it has more than two adjacent empty cells, it's considered as node. 
 % it adds adjacent cells to waiting lists when it hasn't been explored.
 % after add adjacent cells to the waiting list, add the node to explored_node.
 
+queue_waiting_list(Entities, State, State) :-
+    State = state(Move_queue, [], Waiting_list, Exploered_nodes);
+    State = state(Move_queue, Available_agents, [], Exploered_nodes).
+queue_waiting_list(Entities, State, Updated_State)
+    State = state(Move_queue, Available_agents, Waiting_list, Exploered_nodes),
+    Waiting_list = [path(Position | Direction) | Other_waiting_list],
+    Available_agents = [First_agent | Other_agents],
+    ailp_grid_size(N),
+    Max_energy is N*N,
+    heuristic(Position, go(Position), H),
+    solve_task_as(go(Position), go(Position), Max_energy, Max_energy, [state([Position], H)], [], [], [move_queue(_, Reversed_path)]),
+    reverse(Reversed_path, [_| Path]),
+    queue_waiting_list([entity(First_agent, Direction)|Entities], 
+        state([agent_move_queue(First_agent, [Path])|Move_queue], Other_agents, Other_waiting_list, [Position|Exploered_nodes]), Updated_State).
+
+
 % calculate queue and add it to move queue, [agent_move_queue(ID, [Path])]
 % need to get current move queue as well, return the updated move queue.
-% if there is waiting list and if there is Available_agents, allocate available agent to waiting list traveling
-
-queue_waiting_list([], Available_agents, Move_queue, [], Move_queue).
-queue_waiting_list(Waiting_list, [], Move_queue, Waiting_list, Move_queue). 
-queue_waiting_list(Waiting_list, Available_agents, Move_queue, Updated_waiting_list, Updated_move_queue) :-
-    Waiting_list = [Most_recent_found | Waiting_list_left],
-    Available_agents = [First_agent | Other_agents],
-    calculate queue for the agent to go to the cell on the waiting list
-
-
-
 % translate move for one html tick then execute
 execute_explore_queue([], Agent_list, Move_list, []) :-
     agents_do_moves(Agent_list, Move_list).
