4bd54f2d075e09ff1f7e3b42c71e9888f59cc05e
JeeIn-Park <1700pji@naver.com>
2024-08-18 11:20:11 +0100

    file organisation

---
+++ doc/cw_2176132.pl
@@ -0,0 +1,112 @@
+% Accomplish a given task and return the cost
+% solve_task(+Original_task, -Cost)
+solve_task(Original_task,Cost) :-
+    % check the agent for the task and get the initial state
+    my_agent(A), 
+    get_agent_position(A, P),
+    get_agent_energy(A, E),
+    ailp_grid_size(N),
+    % calculate value needed to caculate the path for the task
+    heuristic(P, Original_task, H),
+    X is ( N * N / 4 ), ceiling(X, Max_energy),
+    % New_energy is E - 10, need to deal with query energy
+    solve_task_as(Original_task, Original_task, E, Max_energy, [state([P], H)], [], [], Move_queue), 
+    agent_do_move_queue(A, Move_queue, 0, Cost).
+
+
+% Calculate the path required to achieve a Task
+% solve_task_as(+Final task, +Current Task, +Input energy, +Queue(path), +Visited node, +Current_move_queue, -Move_queue)
+solve_task_as(Original_task, Task, E, Max_energy, Path, Visited_node, Current_move_queue, Move_queue) :-
+    Path = [state(Current_path, _) | Other_states],
+    Current_path = [Current_Position|_],
+    length(Current_path, Estimate_energy_consumption),
+    Estimate_energy is E - Estimate_energy_consumption + 1,
+
+    (Estimate_energy < 0 % check if it runs out of energy
+    ->  (Task = find(c(_))
+        ->  !, fail % run out of energy, fail to find the nearest charging station
+        ;   reverse(Current_path, [Search_starting_position|_]), % run out of energy, try to find the nearest charging station
+            solve_task_as(Original_task, find(c(_)), E, Max_energy, [state([Search_starting_position], 0)], [], Current_move_queue, Move_queue))
+            
+    ;  (achieved(Task, Current_Position)% when it achieved the current goal without running out of energy
+        ->  (task_achieved(Original_task, Current_Position) % check if the final goal is achieved 
+            ->  !, reverse([move_queue(Task, Current_path) | Current_move_queue], Move_queue)
+            ;   Current_path = [New_starting_position|_], % if it was a subgoal, start a search again from the last location
+                heuristic(New_starting_position, Original_task, H),
+                solve_task_as(Original_task, Original_task, Max_energy, Max_energy, [state([New_starting_position], H)], [], [move_queue(Task, Current_path) | Current_move_queue], Move_queue)
+            )
+            
+            % to []
+        ;   (findall(state(New_state_element, F), ( % if it didn't achieve the goal, continue finding a path for the current goal
+                    New_state_element = [New_position|Current_path],
+                    map_adjacent(Current_Position, New_position, empty),
+                    heuristic(New_position, Task, H),
+                    length(Current_path, G),
+                    F is H + G,
+                    \+ member(New_position, Visited_node),
+                    \+ member(state([New_position|_], _), Path)
+                ), New_states) 
+            ->  format('new states : ~w~n', [New_states]),(merge_and_sort_by_heuristic(Other_states, New_states, Priority_queue),
+                solve_task_as(Original_task, Task, E, Max_energy, Priority_queue, [Current_Position|Visited_node], Current_move_queue, Move_queue))
+            ;   format('findall fail'),solve_task_as(Original_task, Task, E, Max_energy, Other_states, Visited_node, Current_move_queue, Move_queue)
+            )
+        )
+    ).
+
+
+
+%heuristic(+Next_position, +Task, -Heuristic)
+heuristic(Next_position, Task, Heuristic) :-
+    ( Task = go(Goal_position), ground(Goal_position), !, map_distance(Next_position, Goal_position, Heuristic)
+    ; Heuristic = 0 ).
+
+
+
+% insert(+Path,+Next,-Priority_queue)
+insert([], State, [State]).
+insert([state(Path, H) | Rest], state(New_path, NH), Priority_queue) :-
+    (H >= NH 
+    ->  Priority_queue = [state(New_path, NH), state(Path, H) | Rest]
+    ;   insert(Rest, state(New_path, NH), Result),
+        Priority_queue = [state(Path, H)|Result]).
+
+
+% merge_and_sort_by_heuristic(+Other_states,+New_states,-Priority_queue)
+merge_and_sort_by_heuristic(Path,[], Path).
+merge_and_sort_by_heuristic(Path, [Next | Rest], Priority_queue) :-
+    insert(Path, Next, Updated),
+    merge_and_sort_by_heuristic(Updated, Rest, Priority_queue).
+
+
+
+
+% achieved(+Task,+Pos)
+achieved(go(Pos), Pos).
+achieved(find(Obj), Pos) :-
+    map_adjacent(Pos, _, Obj),
+    ( Obj = o(ID) 
+    ->  my_agent(A), \+ agent_check_oracle(A, o(ID))
+    ;   true 
+    ).
+
+
+% task_achieved(+Original_task, +Destination)
+task_achieved(Original_task, Destination) :- 
+    Original_task = find(Obj), map_adjacent(Destination,_,Obj)
+    ;   Original_task = go(Destination).
+
+
+% agent_do_move_queue(+A, +Move_queue, +Current_cost, -Cost).
+agent_do_move_queue(_, [], Current_cost, Cost) :-
+    Cost = Current_cost.
+agent_do_move_queue(A, [Current_move|Rest_move_queue], Current_cost, Cost) :-
+    Current_move = move_queue(Task, Reversed_path),
+    reverse(Reversed_path, [_| Path]),
+    length(Path, New_cost),
+    agent_do_moves(A, Path),
+    (Task = find(c(N)) 
+    ->  agent_topup_energy(A, c(N)),
+        New_current_cost is Current_cost + New_cost,
+        agent_do_move_queue(A, Rest_move_queue, New_current_cost, Cost)
+    ;   New_current_cost is Current_cost + New_cost,
+        agent_do_move_queue(A, Rest_move_queue, New_current_cost, Cost)).
---
+++ doc/cw_wp_2176132.pl
@@ -0,0 +1,181 @@
+oracle.
+station.
+:- dynamic object/2.
+:- dynamic max_energy/1.
+:- dynamic query_energy/1.
+
+
+assert_object([]).
+assert_object([Object|Other]) :-
+    assert(Object),
+    assert_object(Other).
+
+
+map_reading :- 
+    retractall(object(_,_)),
+    ailp_grid_size(N),
+    findall(object(o(ID), p(X, Y)),
+        (between(1, N, X), between(1, N, Y), lookup_pos(p(X, Y), o(ID))),
+        Oracles),
+    format('oracles : ~w~n', [Oracles]),
+    assert_object(Oracles),
+    findall(object(c(ID), p(X, Y)),
+        (between(1, N, X), between(1, N, Y), lookup_pos(p(X, Y), c(ID))),
+        Stations),
+    format('stations : ~w~n', [Stations]),
+    assert_object(Stations).
+
+
+% True if link L appears on A's Wikipedia page
+actor_has_link(L, A) :- 
+    actor(A), wp(A, WT), wt_link(WT, L).
+
+
+merge_and_sort_by_distance(Cells, [], Cells).
+merge_and_sort_by_distance(Cells, [O | Os], Sorted_cells) :-
+    insert_cell(Cells, O, Updated),
+    merge_and_sort_by_distance(Updated, Os, Sorted_cells).
+
+
+insert_cell([], State, [State]).
+insert_cell([cell(Distance, Object, Reversed_path) | Rest], cell(New_distance, New_object, New_reversed_path), Sorted_cells) :-
+    (Distance >= New_distance 
+    ->  Sorted_cells = [cell(New_distance, New_object, New_reversed_path), cell(Distance, Object, Reversed_path) | Rest]
+    ;   insert_cell(Rest, cell(New_distance, New_object, New_reversed_path), Result),
+        Sorted_cells = [cell(Distance, Object, Reversed_path) | Result]).
+
+
+adjacent_empty_cell(Location, From, Nearest, Distance) :-
+    findall(Distance-Destination,
+        (map_adjacent(Location, Destination, empty), map_distance(From, Destination, Distance)),
+        Positions),
+    keysort(Positions, Sorted),
+    Sorted = [_-Nearest | _].
+
+
+
+find_nearest(From, oracle, Energy, Destination, ID, Path, Cost) :-
+    format('find nearest oracle~n'),
+    findall(Map_distance-ID,
+        ( object(o(ID), Location),
+        map_distance(From, Location, Map_distance)
+        ),Oracles),
+        format('oracles : ~w~n', [Oracles]),
+    keysort(Oracles, Sorted_Oracles),
+    nearest_oracle(From, Sorted_Oracles, Energy, Destination, ID, Path, Cost).
+
+find_nearest(From, station, Energy, Destination, ID, Path, Cost) :-
+    format('find nearest station~n'),
+    findall(Map_distance-ID,
+        ( object(c(ID), Location),
+        map_distance(From, Location, Map_distance)
+        ),Stations),
+    keysort(Stations, Sorted_stations),
+    nearest_station(From, Sorted_stations, Energy, Destination, ID, Path, Cost).
+
+find_nearest_oracle(From, Energy, ID, Cost) :-
+    format('go nearest oracle~n'),
+    findall(Map_distance-ID,
+        ( object(o(ID), Location),
+        map_distance(From, Location, Map_distance)
+        ),Oracles),
+    keysort(Oracles, Sorted_Oracles),
+    go_nearest_oracle(From, Sorted_Oracles, Energy, ID, Cost).
+
+
+nearest_oracle(From, [_-ID|Oracles], Energy, Destination, ID, Path, Cost) :-
+    format('nearest_oracle | find nearest oracle~n'),
+    object(o(ID), Location),
+    max_energy(Max_energy),
+    adjacent_empty_cell(Location, From, Adj_location, _),
+    ( solve_task_as(go(Adj_location), go(Adj_location), Energy, Max_energy, [state([From], 0)], [], [], [move_queue(go(Adj_location), Reversed_path)])
+    ->  Reversed_path = [Destination|_],
+        reverse(Reversed_path, [_|Path]),
+        length(Path, Cost)
+    ;   nearest_oracle(From, [Oracles], Energy, Destination, ID, Path, Cost)
+    ).
+
+
+nearest_station(From, [_-ID|Oracles], Energy, Destination, ID, Path, Cost) :-
+    format('nearest_station | find nearest station~n'),
+    object(c(ID), Location),
+    max_energy(Max_energy),
+    adjacent_empty_cell(Location, From, Adj_location, _),
+    ( solve_task_as(go(Adj_location), go(Adj_location), Energy, Max_energy, [state([From], 0)], [], [], [move_queue(go(Adj_location), Reversed_path)])
+    ->  Reversed_path = [Destination|_],
+        reverse(Reversed_path, [_|Path]),
+        length(Path, Cost)
+    ;   nearest_station(From, [Oracles], Energy, Destination, ID, Path, Cost)
+    ).
+
+go_nearest_oracle(From, [_-ID|Oracles], Energy, ID, Cost) :-
+    format('go_nearest_oracle | find nearest oracle~n'),
+    object(o(ID), Location),
+    adjacent_empty_cell(Location, From, Adj_location, _), !,
+    (solve_task(go(Adj_location), Cost) ; go_nearest_oracle(From, Oracles, Energy, ID, Cost)).
+ 
+
+eliminate(_, [A], A) :- 
+    format('eliminate | found identity : ~w~n', [A]), !.
+eliminate(G, [A|As], Result) :-
+    query_energy(Query_energy),
+    get_agent_energy(G, E),
+    get_agent_position(G, P),
+    format('eliminate | agent energy [~w], at position [~w] ~n', [E, P]),
+    (find_nearest(P, oracle, E, Destination, OID, Path, Cost)
+    ->  New_energy is E - Cost - Query_energy,
+        format('eliminate | new energy : ~w~n', [New_energy]),
+        ( find_nearest(Destination, station, New_energy, _, _, _, _)
+        ->  agent_do_moves(G, Path),
+            format('eliminate |  --- asking [~w]~n', [OID]),
+            ( \+ agent_check_oracle(G, o(OID))
+            ->  agent_ask_oracle(G, o(OID), link, L),
+                format('eliminate | --- link : ~w~n', [L]), 
+                include(actor_has_link(L), [A|As], New_As),
+                retractall(object(o(OID),_))
+            ;   retractall(object(o(OID),_))
+            ), eliminate(G, New_As, Result)
+        ;   format('eliminate | will not be able to go charging station.. ~n'),
+            ( find_nearest(P, station, E, _, CID, Charge_path, _)
+            ->  agent_do_moves(G, Charge_path),
+                agent_topup_energy(G, c(CID)),
+                format('eliminate | going to charge first ~n'),
+                eliminate(G, [A|As], Result)
+            ;   format('eliminate | cannot go to charge station, visit oracle ~n'),
+                agent_do_moves(G, Path),
+                ( \+ agent_check_oracle(G, o(OID))
+                ->  agent_ask_oracle(G, o(OID), link, L),
+                    include(actor_has_link(L), [A|As], New_As),
+                    retractall(object(o(OID),_))
+                ;   retractall(object(o(OID),_))
+                ), eliminate(G, New_As, Result)
+            )  
+        )
+    ;   format('eliminate | cannot reach oracle, try charging~n'),
+        find_nearest_oracle(P, E, OID, _),
+        ( \+ agent_check_oracle(G, o(OID))
+        ->  agent_ask_oracle(G, o(OID), link, L),
+            format('eliminate | --- link : ~w~n', [L]), 
+            include(actor_has_link(L), [A|As], New_As),
+            retractall(object(o(OID),_))
+        ;   retractall(object(o(OID),_))
+        ), eliminate(G, New_As, Result)
+    ).
+
+% Deduce the identity of the secret actor A
+find_identity(A) :- 
+    ailp_grid_size(N),
+    X is ( N * N / 4 ), ceiling(X, Max_energy),
+    retractall(max_energy(_)),
+    assert(max_energy(Max_energy)),
+    Y is ( Max_energy / 10 ), ceiling(Y, Query_energy),
+    retractall(query_energy(_)),
+    assert(query_energy(Query_energy)),
+    format('find_identity | going to check agent identity... ~n'),
+    map_reading,
+    format('find_identity | >> map reading done << ~n'),
+    findall(A, actor(A), As), 
+    format('find_identity | possible identity list : ~w~n', [As]),
+    my_agent(G),
+    format('find_identity | identity checking just started. ~n'),
+    eliminate(G, As, A).
